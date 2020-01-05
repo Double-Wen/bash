44680e317956f7dad59d3bb9b62f144903d5cbd0
DoubleWen <tcliuwenwen@126.com>
2020-01-05 16:36:29 +0800

    fix changesource.sh

--- changesource.sh
+++ changesource.sh
@@ -1,5 +1,5 @@
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.dw.bak
-wget https://bash.liuwenwen.net/sources.list -O /tmp
+wget https://bash.liuwenwen.net/sources.list -O /tmp/sources.list
 sudo mv /tmp/sources.list /etc/apt/sources.list
 sudo apt update
 
